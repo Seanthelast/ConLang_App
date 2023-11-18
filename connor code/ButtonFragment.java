1757c413229510910dc1cb294357b19674b41a92
conreebar <89880452+conreebar@users.noreply.github.com>
2023-11-18 15:45:52 -0700

    Here's my code so far
    I will keep getting as much frontend stuff as I can done today

---
+++ connor code/ButtonFragment.java
@@ -0,0 +1,76 @@
+package com.example.buttonstest.fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.buttonstest.GridSpacingItemDecoration;
+import com.example.buttonstest.MainActivity;
+import com.example.buttonstest.R;
+
+public class ButtonFragment extends Fragment implements MainActivity.View {
+    private RecyclerView recyclerView;
+    private ButtonAdapter buttonAdapter;
+    private int size;
+    private int offset;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.button_fragment, container, false);
+        TextView textView = view.findViewById(R.id.bottom_button);
+        textView.setText("IPA Sound Selector");
+
+        //get args
+        Bundle args = getArguments();
+        if (args != null) {
+            size = args.getInt("size");
+            offset = args.getInt("offset");
+        }else{
+            Log.d("Problem","args");
+        }
+
+        recyclerView = view.findViewById(R.id.button_recycler);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 4)); // Set span count to 4 for 4 columns
+        buttonAdapter = new ButtonAdapter(offset,size);
+        recyclerView.setAdapter(buttonAdapter);
+
+        int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.grid_spacing); // Customize the spacing dimension
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(spacingInPixels));
+
+        //new button on bottom
+        Button bottomButton = view.findViewById(R.id.bottom_button);
+        bottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SelectedButtonFragment frag = new SelectedButtonFragment();
+
+                FragmentManager fm = requireActivity().getSupportFragmentManager();
+                fm.beginTransaction().replace(
+                                R.id.fragmentContainerView,
+                                frag).commit();
+            }
+        });
+
+        return view;
+    }
+
+    //new instance
+    public static ButtonFragment newInstance(int offset, int size) {
+        ButtonFragment fragment = new ButtonFragment();
+        Bundle args = new Bundle();
+        args.putInt("offset", offset);
+        args.putInt("size", size);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+}
